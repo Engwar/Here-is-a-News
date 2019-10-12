a10cb3b1dc6f9d73b88afbfbae051c23f5136438
Igor Shelginskiy <engwar@yandex.ru>
2019-10-12 13:13:29 +0300

    не парсится JSON(

--- Here is a News/NewsTableViewController.swift
+++ Here is a News/NewsTableViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class NewsTableViewController: UITableViewController {
     
-    var news = [News]()
+    var header = Articles()
+
     var cellManager = CellManager()
 
     override func viewDidLoad() {
@@ -21,42 +22,44 @@ class NewsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var num = news.count
-        if news.count > 20 {
+        var num = header.articles.count
+        if header.articles.count > 20 {
             num = 20
         }
         return num
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let new = news[indexPath.row]
+        let new = header.articles[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsCell
         cellManager.configure(cell, with: new)
         return cell
     }
     
     func getData() {
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=e2c238227b3f4437b4fa4dd7fb395789")!
+        let baseUrl = URL(string: "https://newsapi.org/v2/top-headlines")!
+        
+        let query = ["country": "us", "apiKey": "e2c238227b3f4437b4fa4dd7fb395789"]
+        
+        let url = baseUrl.withQueries(query)!
+        
         let task = URLSession.shared.dataTask(with: url) { data, _, _ in
             guard let data = data else { print("Data has not converted"); return }
-            
-            let jsonDecoder = JSONDecoder()
-            
-            guard let report = try? jsonDecoder.decode(News.self, from: data) else { print("Data cannot be decoded as JSON"); return}
+            let decoder = JSONDecoder()
+            //decoder.dateDecodingStrategy = .iso8601
+            guard let report = try? decoder.decode(Articles.self, from: data) else { print("Data cannot be decoded as JSON"); return}
             
                 print(report)
-            self.news = [report]
-            }
-    
+        }
         task.resume()
     }
 
 }
 
-//extension URL {
-//    func withQueries (_ queries: [String: String]) -> URL? {
-//        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
-//        components?.queryItems = queries.map {URLQueryItem(name: $0.key, value: $0.value)}
-//        return components?.url
-//    }
-//}
+extension URL {
+    func withQueries (_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map {URLQueryItem(name: $0.key, value: $0.value)}
+        return components?.url
+    }
+}
